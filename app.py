8b13f5f3b3c9333d975e23c7a059bfbfd9122e0e
kekekeo <kekekeo532@gmail.com>
2024-12-16 20:26:30 +0800

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#載入LineBot所需要的套件
+# 載入 LineBot 所需要的套件
 from flask import Flask, request, abort
 
 from linebot import (
@@ -11,14 +11,13 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 import re
+
 app = Flask(__name__)
 
-# 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('cAotsZRc95h5IyQ4rNARm6uLKeuQMbxSr4Db80COuW8XQRUZozunXkisl2zpkYnUxmDhMX8yNwSXinTGJFZKkqcfPhjnoLVXZnGlAgpWyY9iYLtaKSbPI6NgOLL+B4q61peSVuEzMKTK3pPLLtGEigdB04t89/1O/w1cDnyilFU=')
-# 必須放上自己的Channel Secret
-handler = WebhookHandler('d54bba779290dff794c281594d11c051')
-
-line_bot_api.push_message('U262565b00a73c456ebba11b0bd1e7762', TextSendMessage(text='你可以開始了'))
+# 必須放上自己的 Channel Access Token
+line_bot_api = LineBotApi('kU4ar4S8kbgto4jk3y0N57JPsB5pRxWvI4yNkj31m1uqZaFtIvJnptg7J48moDnXB90oGVcNLO1nwiFwAQx+klALKYS6ACnWsdF8qYQe+4Ae+4gOlTrE7ww/M88SECkzylc7Cl6xTyijyFcvQdqrwgdB04t89/1O/w1cDnyilFU=')
+# 必須放上自己的 Channel Secret
+handler = WebhookHandler('9c842edd49ecf2de4d59f754e476a760')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -38,39 +37,41 @@ def callback():
 
     return 'OK'
 
-#訊息傳遞區塊
-##### 基本上程式編輯都在這個function #####
+# 訊息傳遞區塊
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = text=event.message.text
-    if re.match('告訴我秘密',message):
-        image_carousel_template_message = TemplateSendMessage(
-            alt_text='這是TemplateSendMessage',
-            template=ImageCarouselTemplate(
-                columns=[
-                    ImageCarouselColumn(
-                        image_url='https://i.imgur.com/kNBl363.jpg',
-                        action=PostbackAction(
-                            label='台灣',
-                            display_text='台北101、逢甲夜市、墾丁...',
-                            data='action=001'
-                        )
-                    ),
-                    ImageCarouselColumn(
-                        image_url='https://i.imgur.com/GBPcUEP.png',
-                        action=PostbackAction(
-                            label='日本',
-                            display_text='金閣寺、淺草寺、北海道...',
-                            data='action=002'
-                        )
-                    )
-                ]
-            )
+    message = event.message.text
+
+    if message == "推薦餐廳":
+        imagemap_message = ImagemapSendMessage(
+            base_url='https://i.imgur.com/your_image_id',  # 請替換為您的圖片 URL
+            alt_text='推薦餐廳',
+            base_size=BaseSize(height=1040, width=1040),
+            actions=[
+                URIImagemapAction(
+                    link_uri='https://www.google.com/maps/search/%E6%97%A5%E5%BC%8F%E6%96%99%E7%90%86',
+                    area=ImagemapArea(x=0, y=0, width=520, height=520)
+                ),
+                URIImagemapAction(
+                    link_uri='https://www.google.com/maps/search/%E8%A5%BF%E5%BC%8F%E6%96%99%E7%90%86',
+                    area=ImagemapArea(x=520, y=0, width=520, height=520)
+                ),
+                URIImagemapAction(
+                    link_uri='https://www.google.com/maps/search/%E4%B8%AD%E5%BC%8F%E6%96%99%E7%90%86',
+                    area=ImagemapArea(x=0, y=520, width=520, height=520)
+                ),
+                URIImagemapAction(
+                    link_uri='https://www.google.com/maps/search/%E6%B3%95%E5%BC%8F%E6%96%99%E7%90%86',
+                    area=ImagemapArea(x=520, y=520, width=520, height=520)
+                )
+            ]
         )
-        line_bot_api.reply_message(event.reply_token, image_carousel_template_message)
+        line_bot_api.reply_message(event.reply_token, imagemap_message)
+
     else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
-#主程式
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+
+# 主程式
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
